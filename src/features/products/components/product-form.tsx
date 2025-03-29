0cb11e090768d11cf20d651a7009362050c3741d
Ohm0025 <toyohm0025@gmail.com>
2025-03-29 20:11:07 +0700

    add:product-form

---
+++ src/features/products/components/product-form.tsx
@@ -0,0 +1,217 @@
+"use client";
+
+import InputForm from "@/components/shared/InputForm";
+import SubmitBtn from "@/components/shared/SubmitBtn";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import { Textarea } from "@/components/ui/textarea";
+import { useForm } from "@/hooks/useForm";
+import { CategoryType } from "@/types/category";
+import { Save } from "lucide-react";
+import Form from "next/form";
+import React, { useState } from "react";
+import { productAction } from "../actions/products";
+import ErrorMessage from "@/components/shared/errorMessage";
+
+interface ProductFormProps {
+  categories: CategoryType[];
+}
+
+const ProductForm = ({ categories }: ProductFormProps) => {
+  const [basePrice, setBasePrice] = useState("");
+  const [salePrice, setSalePrice] = useState("");
+
+  const { errors, formAction, isPending, clearErrors } = useForm(
+    productAction,
+    "/admin/products"
+  );
+
+  const calculateDiscount = () => {
+    const basePriceNum = parseFloat(basePrice) || 0;
+    const salePriceNum = parseFloat(salePrice) || 0;
+
+    if (basePriceNum === 0 || salePriceNum === 0) {
+      return "0.00%";
+    }
+    if (basePriceNum <= salePriceNum) {
+      return "0.00%";
+    }
+
+    const discount = ((basePriceNum - salePriceNum) * 100) / basePriceNum;
+
+    return `${discount.toFixed(2)}%`;
+  };
+
+  return (
+    <Card className="max-w-4xl mx-auto">
+      <CardHeader>
+        <CardTitle className="text-lg sm:text-xl">
+          Product Information
+        </CardTitle>
+        <CardDescription>Enter the details of your new product</CardDescription>
+      </CardHeader>
+
+      <Form
+        action={formAction}
+        onChange={clearErrors}
+        className="flex flex-col gap-4">
+        <CardContent className="flex flex-col gap-6">
+          {/* Basic Information */}
+          <div className="flex flex-col gap-4">
+            <h3 className="font-medium">Basic Information</h3>
+            <div className="flex flex-col gap-2">
+              <InputForm
+                label="Product Title"
+                id="title"
+                placeholder="Enter product title"
+                required
+              />
+              {errors.title && (
+                <ErrorMessage error={errors.title[0]}></ErrorMessage>
+              )}
+            </div>
+
+            {/* Product Description */}
+            <div className="flex flex-col gap-2">
+              <Label htmlFor="description">
+                Description <span className="text-red-500">*</span>
+              </Label>
+              <Textarea
+                className="min-h-20"
+                id="description"
+                name="description"
+                placeholder="Enter description product"></Textarea>
+              {errors.description && (
+                <ErrorMessage error={errors.description[0]} />
+              )}
+            </div>
+
+            {/* Category select */}
+            <div className="flex flex-col gap-2">
+              <Label>
+                Category
+                <span className="text-red-500">*</span>
+              </Label>
+              <Select name="category-id">
+                <SelectTrigger className="w-full">
+                  <SelectValue placeholder="Select Category" />
+                </SelectTrigger>
+
+                <SelectContent>
+                  {categories
+                    .filter((c) => c.status === "Active")
+                    .map((category, index) => (
+                      <SelectItem key={index} value={category.id}>
+                        {category.name}
+                      </SelectItem>
+                    ))}
+                </SelectContent>
+              </Select>
+              {errors.categoryId && (
+                <ErrorMessage error={errors.categoryId[0]} />
+              )}
+            </div>
+          </div>
+
+          {/* Pricing Information */}
+          <div className="flex flex-col gap-4">
+            <h3 className="font-medium">Pricing Information</h3>
+            <div className="grid grid-cols-1 sm:grid-cols-2 gap-4">
+              {/* Cost */}
+              <div className="flex flex-col gap-2">
+                <InputForm
+                  label="Cost Price"
+                  id="cost"
+                  type="number"
+                  min="0"
+                  step="0.01"
+                  placeholder="0.00"
+                />
+                {errors.cost && <ErrorMessage error={errors.cost[0]} />}
+              </div>
+              {/* Base Price */}
+              <div>
+                <div className="flex flex-col gap-2">
+                  <InputForm
+                    label="Base Price"
+                    id="base-price"
+                    type="number"
+                    min="0"
+                    step="0.01"
+                    placeholder="0.00"
+                    required
+                    value={basePrice}
+                    onChange={(e) => setBasePrice(e.target.value)}
+                  />
+                  {errors.basePrice && (
+                    <ErrorMessage error={errors.basePrice[0]} />
+                  )}
+                </div>
+              </div>
+              {/* Sale Price */}
+              <div>
+                <div className="flex flex-col gap-2">
+                  <InputForm
+                    label="Sale Price"
+                    id="price"
+                    type="number"
+                    min="0"
+                    step="0.01"
+                    placeholder="0.00"
+                    required
+                    value={salePrice}
+                    onChange={(e) => setSalePrice(e.target.value)}
+                  />
+                  {errors.price && <ErrorMessage error={errors.price[0]} />}
+                </div>
+              </div>
+              {/* Discount % */}
+              <div className="flex flex-col gap-2">
+                <Label>Discount</Label>
+                <div className="text-sm text-muted-foreground h-9 px-3 rounded-md border border-input bg-gray-50 flex items-center">
+                  {calculateDiscount()}
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* Stock Information */}
+          <div className="flex flex-col gap-4">
+            <h3 className="font-medium">Stock Information</h3>
+            <div>
+              <InputForm
+                label="Stock Quantity"
+                id="stock"
+                type="nunmber"
+                min="0"
+                placeholder="0"
+                required
+              />
+              {errors.stock && <ErrorMessage error={errors.stock[0]} />}
+            </div>
+          </div>
+        </CardContent>
+
+        <CardFooter>
+          <SubmitBtn name="Save Product" icon={Save} pending={isPending} />
+        </CardFooter>
+      </Form>
+    </Card>
+  );
+};
+
+export default ProductForm;
--- src/features/products/db/products.ts
+++ src/features/products/db/products.ts
@@ -1,6 +1,30 @@
 import { db } from "@/lib/db";
+import {
+  unstable_cacheLife as cacheLife,
+  unstable_cacheTag as cacheTag,
+} from "next/cache";
+import { getProductGlobalTag, revalidateProductCache } from "./cache";
+import { createProductSchema } from "../schemas/products";
+import { authCheck } from "@/features/auths/db/auth";
+import { canCreateProduct } from "../permissions/products";
+import { redirect } from "next/navigation";
+
+interface CreateProductInput {
+  title: string;
+  description: string;
+  cost?: number;
+  basePrice: number;
+  price: number;
+  stock: number;
+  categoryId: string;
+}
 
 export const getProducts = async () => {
+  "use cache";
+
+  cacheLife("hours");
+  cacheTag(getProductGlobalTag());
+
   try {
     const products = await db.product.findMany({
       include: {
@@ -17,10 +41,58 @@ export const getProducts = async () => {
     return products.map((product) => ({
       ...product,
       lowStock: 5,
-      sku: product.id.substring(0, 8),
+      sku: product.id.substring(0, 8).toUpperCase(),
     }));
   } catch (error) {
     console.error("Error getting products : ", error);
     return [];
+  }
+};
+
+export const createProduct = async (input: CreateProductInput) => {
+  const user = await authCheck();
+  if (!user || !canCreateProduct(user)) {
+    redirect("/");
+  }
+  try {
+    const { success, data, error } = createProductSchema.safeParse(input);
+
+    if (!success) {
+      return {
+        message: `Please enter valid product information`,
+        error: error.flatten().fieldErrors,
+      };
+    }
+
+    // check if category exist
+    const category = await db.categories.findUnique({
+      where: { id: data.categoryId, status: "Active" },
+    });
+
+    if (!category) {
+      return {
+        message: "Selected category not found or inactive",
+      };
+    }
+
+    // create new product
+    const newProduct = await db.product.create({
+      data: {
+        title: data.title,
+        description: data.description,
+        cost: data.cost,
+        basePrice: data.basePrice,
+        price: data.price,
+        stock: data.stock,
+        categoriesId: data.categoryId,
+      },
+    });
+
+    revalidateProductCache(newProduct.id);
+  } catch (error) {
+    console.error("Error creating product : ", error);
+    return {
+      message: "Something went wrong. Please try again later",
+    };
   }
 };
