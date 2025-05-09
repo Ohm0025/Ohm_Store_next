5cc75848183e15929d51b5ea81abc199bb4b126c
Porramat081 <porramat081@gmail.com>
2025-05-09 20:59:16 +0700

    add:cancel-order-backend

--- src/features/orders/components/cancel-order-modal.tsx
+++ src/features/orders/components/cancel-order-modal.tsx
@@ -5,6 +5,7 @@ import { Ban } from "lucide-react";
 import Form from "next/form";
 import React from "react";
 import { cancelOrderStatusAction } from "../actions/orders";
+import { useForm } from "@/hooks/useForm";
 
 interface CancelOrderModalProps {
   open: boolean;
@@ -13,22 +14,26 @@ interface CancelOrderModalProps {
 }
 
 const CancelOrderModal = (props: CancelOrderModalProps) => {
+  const { formAction, isPending } = useForm(cancelOrderStatusAction);
+
   return (
     <Modal
       title={"Cancel Order"}
       description={"Do you wanna cancel this order?"}
       open={props.open}
       onOpenChange={props.onOpenChange}>
-      <Form action={cancelOrderStatusAction}>
+      <Form action={formAction}>
         <input type="hidden" name="order-id" value={props.orderId} />
         <div className="flex justify-end space-x-2 pt-2">
           <Button
             variant={"outline"}
             type="button"
+            disabled={isPending}
             onClick={() => props.onOpenChange(false)}>
             close
           </Button>
           <SubmitBtn
+            pending={isPending}
             name="confirm cancel"
             icon={Ban}
             className="bg-destructive hover:bg-destructive/80"
